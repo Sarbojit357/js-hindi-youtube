f56c6938e256eb6c2a7bfe546f5cfe80ed08b26c
Sarbojit Podder <113027026+Sarbojit357@users.noreply.github.com>
2024-05-30 20:26:28 +0000

    new

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "sarbojitpodder2003@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+//const cant be changes anymore
+// accountId = 2
+
+
+accountEmail ="hc@hc.com"
+accountPassword ="21221221"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope anf functional scope
+*/

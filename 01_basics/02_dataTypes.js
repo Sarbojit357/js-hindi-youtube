63ffdb5ff4b7ae16f55ba3b83d4658a689cf8706
Sarbojit Podder <113027026+Sarbojit357@users.noreply.github.com>
2024-06-04 21:18:05 +0000

    new commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict";
+// alert("hello") We are using Nodejs not Browser
+
+console.log("Hitesh") //cod ereadibilty should be high
+console.log(3+3)
+console.log("Sarbojit Podder");
+console.log("Podder");

6b80aa1add695518fb7d611d3321c40e390df455
Sarbojit Podder <113027026+Sarbojit357@users.noreply.github.com>
2024-06-05 19:09:08 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,3 +2,7 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+console.log("1"+ 2); //12
+console.log(1+"2"); //12
+console.log("1"+ 2 + 2); //122
+console.log(1 + 2 + "2");

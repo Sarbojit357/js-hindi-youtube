2d1e52c4e10dca05105bfe05c0c2c05e1d9bf0f5
Sarbojit Podder <113027026+Sarbojit357@users.noreply.github.com>
2024-06-05 18:36:43 +0000

    new

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,3 +5,21 @@ console.log("Hitesh") //cod ereadibilty should be high
 console.log(3+3)
 console.log("Sarbojit Podder");
 console.log("Podder");
+
+let name ="hitesh"
+let age ="18"
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object 
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
